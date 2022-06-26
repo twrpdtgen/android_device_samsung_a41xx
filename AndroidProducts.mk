0d39ea99740a0557533934f7c61bc82addd2226e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-06-26 15:22:12 +0200

    a41xx: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-A415F Script version: 2.3.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_a41xx.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_a41xx-user \
+    omni_a41xx-userdebug \
+    omni_a41xx-eng
---
+++ omni_a41xx.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from a41xx device
+$(call inherit-product, device/samsung/a41xx/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a41xx
+PRODUCT_NAME := omni_a41xx
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A415F
+PRODUCT_MANUFACTURER := samsung
